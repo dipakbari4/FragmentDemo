91d434689c5059a46b38f25d215e8e82a35ab2e6
Dipak Bari <dipakbari4@gmail.com>
2021-03-18 16:38:24 +0530

    Fragment adding programmatically

--- app/src/main/java/com/dipak/fragmentdemo/MainActivity.kt
+++ app/src/main/java/com/dipak/fragmentdemo/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.dipak.fragmentdemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+import com.dipak.fragmentdemo.fragments.example.ExampleFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add<ExampleFragment>(R.id.fragment_container_view)
+            }
+        }
     }
 }
